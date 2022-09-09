8810c80031e52ba19e337113a6cc74460eba8146
Santiago Martinez Campari <santidrek@hotmail.com>
2022-09-08 23:40:27 -0300

    Segundo commit

---
+++ apiproductos.js
@@ -0,0 +1,68 @@
+const container = require('./container')
+const express = require("express")
+const { setDefaultResultOrder } = require('dns')
+
+const {Router} = express
+const router = Router()
+
+router.get('/productos', (req, res) => {
+    nuevo.getAll().then(function(result){
+        res.send({array: JSON.parse(result)})
+    })
+})
+
+router.get('/productos/:id', (req , res) => {
+    const {id} = req.params
+    nuevo.getById(id).then(function(result){
+        // comparar con undefined me parece bastante feo, deberia buscar una mejor forma de handlear el error
+        if (result == undefined){
+            res.send({error: "producto no encontrado"})
+        }
+        else{
+            res.send({producto: result})
+        }
+    })
+})
+
+router.post('/productos', (req, res) => {
+    const {title, price, thumbnail} = req.body
+    console.log(title)
+    console.log(price)
+    console.log(thumbnail)
+    //otra vez comparo con undefined
+    if (title != undefined && price != undefined && thumbnail != undefined){
+        var len
+        nuevo.getAll().then(function(result){
+            len = JSON.parse(result).length + 1
+            res.send({producto: {title: title, price: price, thumbnail: thumbnail, "id": len}})
+        })
+        nuevo.save({title, price, thumbnail})
+    }
+    else{
+        res.send({error : "datos incorrectos"})
+    }
+    
+})
+
+router.put('/productos/:id', (req, res) => {
+    const {id} = req.params
+    nuevo.modifyById(id)
+    res.send({respuesta: "archivo modificado"})
+})
+
+router.delete('/productos/:id', (req, res) => {
+    const {id} = req.params
+    nuevo.deleteById(id)
+    res.send({respuesta: "producto borrado"})
+})
+
+module.exports = router;
+
+const nuevo = new container("./productos.txt")
+const guardar = {title: 'Fortaleza', price: '150', thumbnail: 'http'}
+//nuevo.save(guardar)
+//nuevo.getAll().then(value => console.log(value))
+//const primero = nuevo.getById(1)
+//nuevo.deleteById(2)
+//nuevo.deleteAll()
+//primero.then(value => console.log(value))
---
+++ container.js
@@ -0,0 +1,132 @@
+const fs = require("fs")
+
+class Container{
+    constructor (file){
+        this.file = file
+    }
+    
+    save(obj){
+        let content = this.getAll()
+        const path = this.file
+
+        if(obj.title != null && obj.price != null && obj.thumbnail != null){
+            addToFile(obj)
+            console.log("Objeto real")
+        }
+        else{
+            console.error("Objeto invalido")
+        }
+
+        async function addToFile(object){
+            try{
+                let arr = await content
+                arr = JSON.parse(arr)
+                console.log(arr)
+                console.log(arr.length)
+                if(arr.length == 0){
+                    arr = []
+                    object.id = 1
+                }
+                else{
+                    const highIdArr = arr.map(x => x.id)
+                    const max = Math.max.apply(null, highIdArr)
+                    object.id = max + 1
+                    
+                }
+                arr.push(object)
+                const objJson = JSON.stringify(arr)
+                await fs.promises.writeFile(path, objJson)
+                console.log("Object added")
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+    }
+
+    getById(number){
+        let arr = this.getAll()
+        async function wait(){
+            try{
+                arr = await arr
+                arr = JSON.parse(arr)
+                return arr.find(x => x.id == number)
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        return wait()
+    }
+
+    getAll() {
+        const path = this.file
+        return read(path)
+    }
+
+    deleteById(number){
+        let arr = this.getAll()
+        const path = this.file
+        async function del(){
+            try{
+                arr = await arr
+                arr = JSON.parse(arr)
+                arr = arr.filter(x => x.id != number)
+                arr = JSON.stringify(arr)
+                await fs.promises.writeFile(path, arr)
+                console.log("Object deleted")
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        del()
+    }
+
+    modifyById(object, number){
+        let arr = this.getAll()
+        const path = this.file
+        async function mod(){
+            try{
+                arr = await arr
+                arr = JSON.parse(arr)
+                arr = arr.filter(x => x.id != number)
+                arr.push({object, "id": number})
+                arr = JSON.stringify(arr)
+                await fs.promises.writeFile(path, arr)
+                console.log("Object deleted")
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        mod()
+    }
+
+    deleteAll(){
+        const path = this.file
+        async function del(){
+            try{
+                let arr = JSON.stringify([])
+                await fs.promises.writeFile(path, arr)
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        del()
+    }
+}
+
+async function read(file){
+    try{
+        let content = await fs.promises.readFile(file, 'utf-8')
+        const arr = content.split(/\r?\n/)
+        return arr[0]
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = Container
